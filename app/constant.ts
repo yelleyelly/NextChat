f606db3aa84b227796a83cd8b40214d50e480159
yelleyelly <163236263+yelleyelly@users.noreply.github.com>
2025-02-07 11:53:44 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -109,7 +109,7 @@ export const UNFINISHED_INPUT = (id: string) => "unfinished-input-" + id;
 
 export const STORAGE_KEY = "chatgpt-next-web";
 
-export const REQUEST_TIMEOUT_MS = 60000;
+export const REQUEST_TIMEOUT_MS = 600000;
 
 export const EXPORT_MESSAGE_CLASS_NAME = "export-markdown";
 
